e0eb862a56cee573b4dd12f4851a59831e10fe60
gioner.x <122662305+gionerx@users.noreply.github.com>
2023-02-22 12:46:44 -0500

    Create Datos_simples.py

---
+++ Datos_simples.py
@@ -0,0 +1,44 @@
+#En python se utilizan tres tipos de datos (String, Numbers and Boolean)
+
+#Los string son cadenas de textos
+
+my_string1 = 'La programación es lo mejor del mundo.'
+my_string2 = 'Hello world!'
+my_string3= 'Hello Python!'
+
+#las cadenas de textos se escriben con: "",'',""""
+
+#Los datos númericos, se clasifican en (int, float y complex)
+
+my_number = 1
+my_number2 = 5
+my_number3 = 10
+
+#Simplemente son números enteros. 
+
+#Los datos del tipo flotante, son números con decimales. 
+
+my_float_number1 = 1.5
+my_float_number2 = 5.5
+my_float_number3 = 6.6
+
+#Los datos complejos, se los mostrare pero en la práctica casi no lo he utilizado.
+
+complex1 = 1j
+complex2 = 5j
+complex3 = 9j
+
+#Datos del tipo booleano, son sacados del algebra de bool para hacer operaciones
+#lógicas donde un se determinan únicamente dos valores discretos (Verdadero y Falso)
+#En este tipo de datos se reduce todo el funcionamiento de la electrónica digital, circuitos e informática.
+
+#True representa en el sistema binario (1) que quiere decir que hay corriente 
+#y por lo tanto se transportan los datos.
+
+n = True 
+m = True
+
+#False reprensenta (0) aunsencia de tensión y por lo tanto no hay datos.
+
+x = False
+y = False
